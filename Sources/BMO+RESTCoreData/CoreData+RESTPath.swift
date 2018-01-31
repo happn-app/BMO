3fa375b341b68f70d61d930f15e6f075edb6b0a9
Frizlab <fload@me.com>
2018-01-31 13:46:59 +0100

    Disable RESTPathKeyResovable extension for NSManagedObject
    The extension is not “seen” by clients linking to BMO_RESTCoreData, so
    clients will have to implement it themselves :(

--- Sources/BMO+RESTCoreData/CoreData+RESTPath.swift
+++ Sources/BMO+RESTCoreData/CoreData+RESTPath.swift
@@ -12,11 +12,19 @@ import RESTUtils
 
 
 
-extension NSManagedObject : RESTPathKeyResovable {
-	
-	public func restPathObject(for key: String) -> Any? {
-		guard entity.propertiesByName.keys.contains(key) else {return nil}
-		return value(forKey: key)
-	}
-	
-}
+/* Ideally this extension should be uncommented here and clients that link to
+ * BMO_RESTCoreData would benefit from it directly.
+ * I tried, it does not seem to work! (Xcode 9.2, (9C40b), default Swift
+ * toolchain)
+ * Instead, clients will have to put this extension is their code...
+ * Note: If the extension is not commented here, clients won't be able to put it
+ *       in their code as compiler will complain about redeclaring protocol
+ *       conformance for NSManagedObject! */
+//extension NSManagedObject : RESTPathKeyResovable {
+//
+//	public func restPathObject(for key: String) -> Any? {
+//		guard entity.propertiesByName.keys.contains(key) else {return nil}
+//		return value(forKey: key)
+//	}
+//
+//}
