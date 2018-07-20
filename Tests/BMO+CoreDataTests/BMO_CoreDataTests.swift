23247c82104af4e3ae081cf3b16fef913ec0e90e
Thomas Le Gravier <thomas.le-gravier@happn.fr>
2018-07-20 17:06:04 +0200

    Allow « AND » predicate compound for constants enumeration

--- Tests/BMO+CoreDataTests/BMO_CoreDataTests.swift
+++ Tests/BMO+CoreDataTests/BMO_CoreDataTests.swift
@@ -9,21 +9,28 @@
 import XCTest
 @testable import BMO_CoreData
 
+import CoreData
+
 
 
 class BMO_CoreDataTests: XCTestCase {
-	
-	func testExample() {
-		/* This is an example of a functional test case.
-		 * Use XCTAssert and related functions to verify your tests produce the
-		 * correct results. */
-		XCTAssertTrue(true)
+    
+	func testCompoundPredicateConstantsEnumeration() {
+		let expectedKeyPath = ["toto", "titi", "tata"]
+		let predicate = NSPredicate(format: "%K == TRUE AND %K == TRUE AND %K == %@", expectedKeyPath[0], expectedKeyPath[1], expectedKeyPath[2], NSManagedObject())
+		
+		var foundKeyPath = Set<String>()
+		predicate.enumerateFirstLevelConstants(forKeyPath: nil, withAndCompound: true, { keyPath, constant in
+			foundKeyPath.insert(keyPath)
+		})
+		
+		XCTAssertEqual(foundKeyPath, Set(expectedKeyPath))
 	}
 	
 	
 	/* Fill this array with all the tests to have Linux testing compatibility. */
 	static var allTests = [
-		("testExample", testExample)
+		("testCompoundPredicateConstantsEnumeration", testCompoundPredicateConstantsEnumeration)
 	]
 	
 }
