1ee7599f447841903fd1dfd64fb76e0c87cdc40b
Frizlab <fload@me.com>
2020-02-11 14:05:09 +0100

    Finish removing NSPropertyDescription hacks (fully replaced w/ a safer hack)

--- Sources/BMO+CoreData/Utilities/NSPropertyDescriptionHashableWrapper.swift
+++ Sources/BMO+CoreData/Utilities/NSPropertyDescriptionHashableWrapper.swift
@@ -22,7 +22,21 @@ import Foundation
 /**
 CoreData has a (f**ing) bug! It is possible to get two NSPropertyDescription
 objects that are equal but have different hashes. So we wrap the properties to
-override the hash. */
+override the hash.
+
+More details on the bug:
+  - Entity A is parent of entity B and declares the property p;
+  - Asking CoreData the property description for the property p on A
+    or B will both return a valid property description (let's call
+    them p1 & p2);
+  - Both property descriptions returned are equal (p1 == p2 in the
+    Swift sense (not pointer equal, but isEqual: equal));
+  - However, they do **NOT** have the same hash! The property
+    description returned for B is a _proxy_ for the description of the
+    A property description. And apparently, Apple miscalculated the
+    hash in the proxy :(
+This is **NOT** a Swift/ObjC interoperability problem, as the same bug
+has been observed on an ObjC-only project. */
 public struct NSPropertyDescriptionHashableWrapper : Hashable {
 	
 	public var wrappedProperty: NSPropertyDescription
